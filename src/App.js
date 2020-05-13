6be1ab9b9c88d36e6cc0f964023fc2a786a4553f
Ryan Chen <juoyang@gatech.edu>
2020-05-13 18:44:41 +0800

    header and footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Footer from './components/Footer';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Juo-Yang Chen",
+      headerLinks: [
+        {title: "Home", path: "/"},
+        {title: "About", path: "/about"},
+        {title: "Contacts", path: "/contact"}
+      ],
+      home: {
+        title: "Welcome to my website."
+      },
+      about: {
+        title: "About Me"
+      },
+      contacts: {
+        title: "Contact Me"
+      }
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Juo-Yang Chen</Navbar.Brand>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Footer />
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;

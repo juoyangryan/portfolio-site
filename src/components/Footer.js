2f040098529370283153e69f0cc51eac1b123236
Ryan Chen <juoyang@gatech.edu>
2020-06-03 01:49:39 +0800

    complete footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,28 +6,38 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import { IconButton } from '@material-ui/core';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@material-ui/icons/Email';
+import Divider from '@material-ui/core/Divider';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import PhoneIcon from '@material-ui/icons/Phone';
 
 function Footer() {
     return(
         <footer className="mt-5">
             <Container fluid={true}>
                 <Col>
+                    <Divider light={false} variant='full width'/>
                     <Row>
                         <div className='footer-icons'>
                             <IconButton href='https://github.com/juoyangryan' target="_blank" rel="noopener noreferrer">
                                 <GitHubIcon />
                             </IconButton>
+                            <p className="d-inline-block divider"> | </p>
                             <IconButton href='https://www.linkedin.com/in/juo-yang-chen-751398192/' target="_blank" rel="noopener noreferrer">
                                 <LinkedInIcon />
                             </IconButton>
-                            <IconButton>
-                                <EmailIcon />
+                            <p className="d-inline-block divider"> | </p>
+                            <IconButton href='https://www.facebook.com/ryan.chen.9440234' target="_blank" rel="noopener noreferrer">
+                                <FacebookIcon />
                             </IconButton>
                         </div>
                     </Row>
                     <Row>
                         <div className="footer-content">
-                            Made With React
+                            <EmailIcon />: juoyangryan@gmail.com
+                            <p className="d-inline-block divider"> | </p>
+                            <PhoneIcon />: +1 (848) 218-8433
+                            <p className="d-inline-block divider"> | </p>
+                            Made with React
                         </div>
                     </Row>
                 </Col>
